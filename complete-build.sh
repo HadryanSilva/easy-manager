0cd61c1392e9462020cf05f75a4c5c8ecfb7ef68
Hadryan Silva <hadryan2001@gmail.com>
2024-07-19 17:37:45 -0300

    docker compose changes + init-db.sql

---
+++ complete-build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git clone https://github.com/HadryanSilva/easy-api.git
+git clone https://github.com/HadryanSilva/easy-notification.git
+git clone https://github.com/HadryanSilva/easy-sales.git
+git clone https://github.com/HadryanSilva/easy-stock.git
+
+##docker compose up -d
---
+++ scripts/init-db.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE easy_sales_db;
+CREATE DATABASE easy_stock_db;
